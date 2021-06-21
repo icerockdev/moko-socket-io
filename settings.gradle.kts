dba47b698d89fe1e4c9356e1644ad462ded77fe4
Anton Shestak <ashestak@icerockdev.com>
2021-06-21 18:12:39 +0700

    #13 sync and help successful

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,33 @@
  * Copyright 2020 IceRock MAG Inc. Use of this source code is governed by the Apache 2.0 license.
  */
 
+enableFeaturePreview("VERSION_CATALOGS")
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        mavenCentral()
+        google()
+
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        google()
+
+        jcenter {
+            content {
+                includeGroup("org.jetbrains.trove4j")
+                includeGroup("org.jetbrains.kotlinx")
+            }
+        }
+    }
+}
+
+
 include(":socket-io")
 include(":sample:android-app")
 include(":sample:mpp-library")
--- socket-io/build.gradle.kts
+++ socket-io/build.gradle.kts
@@ -6,22 +6,22 @@ import java.util.Base64
 import kotlin.text.String
 
 plugins {
-    plugin(Deps.Plugins.androidLibrary)
-    plugin(Deps.Plugins.kotlinMultiplatform)
-    plugin(Deps.Plugins.kotlinAndroidExtensions)
-    plugin(Deps.Plugins.mobileMultiplatform)
-    plugin(Deps.Plugins.mavenPublish)
-    plugin(Deps.Plugins.signing)
+    id("com.android.library")
+    id("org.jetbrains.kotlin.multiplatform")
+    id("kotlin-android-extensions")
+    id("dev.icerock.mobile.multiplatform")
+    id("org.gradle.maven-publish")
+    id("signing")
 }
 
 group = "dev.icerock.moko"
-version = Deps.mokoSocketIoVersion
+version = libs.versions.mokoSocketIoVersion.get()
 
 dependencies {
-    commonMainImplementation(Deps.Libs.MultiPlatform.serialization)
+    commonMainImplementation(libs.serialization)
 
-    androidMainImplementation(Deps.Libs.Android.appCompat)
-    androidMainImplementation(Deps.Libs.Android.socketIo) {
+    androidMainImplementation(libs.appCompat)
+    androidMainImplementation(libs.socketIo) {
         exclude(group = "org.json", module = "json")
     }
 }
